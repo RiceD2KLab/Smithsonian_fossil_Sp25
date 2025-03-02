b3da6098913ed48eb66b3282e9a0855ef826b4bc
Jonathan Lee <jwl9@loginx3.nots.rice.edu>
2025-03-02 15:27:30 -0600

    edits to annotation tiler

--- src/data_preprocessing/annotation_tiling.py
+++ src/data_preprocessing/annotation_tiling.py
@@ -3,13 +3,23 @@ import pandas as pd
 import os
 import csv
 
+"""
+path for current master annotation csv: /projects/dsci435/smithsonian_sp25/data/cleaned_annotations.csv
+all cropped tiles dir: /projects/dsci435/smithsonian_sp25/data/tile_imgs
+output_dir: /projects/dsci435/smithsonian_sp25/data
+nm_to_px_ratio: 229
+tile_size: 1024
+
+Usage: 
+python3 annotation_tiling.py --annotation_filepath /projects/dsci435/smithsonian_sp25/data/cleaned_annotations.csv --all_cropped_tiles_dir /projects/dsci435/smithsonian_sp25/data/tile_imgs --output_dir /projects/dsci435/smithsonian_sp25/data --nm_to_px_ratio 229 --tile_size 1024
+"""
 
 def get_args():
     parser = ArgumentParser(description="get args for tile cropper")
     parser.add_argument("--annotation_filepath", type=str, help="path to the master annotation file")
     parser.add_argument("--all_cropped_tiles_dir", type=str, help="directory to all the tile crops for all .ndpi files")
     parser.add_argument("--output_dir", type=str, help="directory to save the annotation file with all tiles")
-    parser.add_agument("--nm_to_px_ratio", type=float, help="the pixel to nanometer ratio")
+    parser.add_argument("--nm_to_px_ratio", type=float, help="the pixel to nanometer ratio")
     parser.add_argument("--tile_size", type=int, help="the side length (in pixels) of the square tile crop")
     args = parser.parse_args()
     return args
@@ -25,15 +35,15 @@ if __name__ == "__main__":
     with open(master_annotation_filepath, mode="r", newline="") as master_annotation_csv, open(os.path.join(output_dir, "master_annotations_tiled.csv"), mode="w", newline="") as annotations_tiled:
         reader = csv.DictReader(master_annotation_csv)
         writer = csv.writer(annotations_tiled)
-        headers = next(reader)
+        headers = reader.fieldnames
         new_headers = headers + ["tile"]
         writer.writerow(new_headers)
         # iterate over the master annotation file
         for row in reader:
             file_name = row["file"]
-            radius_nano = row["radius"]
-            c_x_nano = row["x"]
-            c_y_nano = row["y"]
+            radius_nano = int(row["radius"])
+            c_x_nano = int(row["x"])
+            c_y_nano = int(row["y"])
             
             # convert center X coordinate, center Y coordinate, and radius from nanometers to pixels
             radius_px = radius_nano / nm_to_px_ratio
