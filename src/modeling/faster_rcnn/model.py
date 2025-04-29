400af393cdea065586ccdb80bddbc5f1a9f3c579
Andrew Ondara <aoo4@loginx2.nots.rice.edu>
2025-04-29 11:58:31 -0500

    WIP

--- src/modeling/faster_rcnn/model.py
+++ src/modeling/faster_rcnn/model.py
@@ -4,12 +4,6 @@ import torchvision
 from torchvision.models.detection import fasterrcnn_resnet50_fpn
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 
-def get_model(num_classes):
-    model = fasterrcnn_resnet50_fpn(pretrained=True)
-    in_features = model.roi_heads.box_predictor.cls_score.in_features
-    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
-    return model
-
 def load_model(model_path, num_classes, device):
     model = get_model(num_classes)
     model.load_state_dict(torch.load(model_path, map_location=device))
