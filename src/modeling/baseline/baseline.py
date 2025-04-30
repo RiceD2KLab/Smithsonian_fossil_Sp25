7f8dcf3063c108b51280849b6b962d3bec384b57
Jonathan Lee <jwl9@loginx3.nots.rice.edu>
2025-04-30 00:53:17 -0500

    modeling/baseline.py not finished

---
+++ src/modeling/baseline/baseline.py
@@ -0,0 +1,24 @@
+"""
+This file will have the following functions:
+- Select 9 planes
+- resize planes
+- save in some massive list output or something. 
+- be able to treat 
+
+
+
+0: should we make a new directory of only 9 focal planes?
+0.5: should we make a new directory of the resized?
+    - this could double up the storage
+    - this could double up the storage 
+0.75: we could edit the code such that instead of iterating throuhg a physical existing directory, it 
+    iterates through a list of the resized images that will be processed. 
+    - not going to do this because we need to do alot of code edits to the specific dataloader. 
+
+    SOOO conclusion, is to prompt the user to give them a new directory to store the edited data for the model. 
+
+
+
+1. def going to keep all the outputs. 
+
+"""
